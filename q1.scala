3e913f3d85c106a2a673f97706adbe24c433dcca
Samitha Rathnayake <106796575+Samitha-001@users.noreply.github.com>
2022-09-19 21:59:12 +0530

    Tutorial 8 was done

--- q1.scala
+++ q1.scala
@@ -1,18 +1,23 @@
 object q1 {
-  class rational(x:Int, y:Int){
+    case class Point(a:Int, b:Int){
+      def x:Int = a
+      def y:Int = b
 
-    def numer:Int = x
-    def denom:Int = y
-
-    def neg = new rational(-this.numer, this.denom)
-
-    def printOut(): Unit ={
-      println(numer + "/" + denom)
+      def +(p:Point): Point = Point(p.x + x, p.y + y)
+      def move(dx:Int , dy:Int): Point = Point(x+dx, y+dy)
+      def distance(p1:Point, p2:Point): Double = Math.sqrt(Math.pow((p2.x-p1.x),2) + Math.pow((p2.y-p1.y),2))
+      def invert(p:Point): Point = Point(p.y, p.x)
     }
+
+  def main(args: Array[String]): Unit = {
+    val p1 = Point(1,2)
+    println(p1+Point(0,2))
+    println(p1.move(1,2))
+    println(p1.distance(Point(1,1), Point(4,5)))
+    println(p1.invert(Point(2,-8)))
+
   }
 
-  def main(args: Array[String]): Unit ={
-    var x = new rational(1,2)
-    x.neg.printOut()
-  }
+
+
 }
